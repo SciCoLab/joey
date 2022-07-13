2668ad9b9692c03fc304099dc0a76f04a980bdc3
utkarsh8028 <utkarsh8028@gmail.com>
2022-07-13 18:41:34 +0000

    added bias 0 for testing

--- tests/2Dconvoltion_test.py
+++ tests/2Dconvoltion_test.py
@@ -34,34 +34,35 @@ torch_conv_op = torch.nn.Conv2d(1,1, kernel_size=3, stride=1)
 
 
 with torch.no_grad():
+    
     torch_conv_op.weight = torch.nn.Parameter(weights)
     torch_conv_op.bias = torch.nn.Parameter(torch.Tensor([0]))
 
 
-print(torch_conv_op(custom_input_T))
+print(torch_conv_op(custom_input_T).detach().numpy())
 
 #Random image size test b/w pyTorch and Joey
 
-weight_dev = torch_conv_op.weight.transpose(0,1).detach().numpy()
+# weight_dev = torch_conv_op.weight.transpose(0,1).detach().numpy()
 
 
-# convert the weights back to numpy so they can be used 
-# in devito
-bias_dev = torch_conv_op.bias.detach().numpy()
+# # convert the weights back to numpy so they can be used 
+# # in devito
+# bias_dev = torch_conv_op.bias.detach().numpy()
 
-# pass the data to the PyTorch operator
-torch_conv_res = torch_conv_op(input_data)
-# Create the Joey Operator
+# # pass the data to the PyTorch operator
+# torch_conv_res = torch_conv_op(input_data)
+# # Create the Joey Operator
 
-layer1 = ml.Conv(kernel_size=(1, 3, 3),
-                        input_size=(1, 1, image_size , image_size),
-                        activation=None,
-                        generate_code=True)
+# layer1 = ml.Conv(kernel_size=(1, 3, 3),
+#                         input_size=(1, 1, image_size , image_size),
+#                         activation=None,
+#                         generate_code=True)
                         
-# pass the data to the Joey operator
-current_data = layer1.execute(input_data,bias_dev,weight_dev)
-print("Same result:", np.allclose(torch_conv_res.detach().numpy(), current_data))
-print("Max error across all indices :", np.max(abs(abs(torch_conv_res.detach().numpy()) - abs(current_data))))
+# # pass the data to the Joey operator
+# current_data = layer1.execute(input_data,bias_dev,weight_dev)
+# print("Same result:", np.allclose(torch_conv_res.detach().numpy(), current_data))
+# print("Max error across all indices :", np.sum(abs(abs(torch_conv_res.detach().numpy()) - abs(current_data))))
 
-print("PyTorch Weight sum:", np.sum(abs(torch_conv_res.detach().numpy())))
-print("Joey Weight sum :", np.sum(abs(current_data)))
+# print("PyTorch Weight sum:", np.sum(abs(torch_conv_res.detach().numpy())))
+# print("Joey Weight sum :", np.sum(abs(current_data)))
