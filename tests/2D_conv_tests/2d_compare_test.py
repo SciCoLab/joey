dc0d028cede1d019a996ff4d2d0bf585c8548049
utkarsh8028 <utkarsh8028@gmail.com>
2022-07-20 23:45:20 +0000

    Stride and convolution working

--- tests/2D_conv_tests/2d_compare_test.py
+++ tests/2D_conv_tests/2d_compare_test.py
@@ -3,30 +3,28 @@ from pyTorch_2Dconv import pyTorch_conv
 from Devito_Conv_2d import *
 import numpy as np
 
-c=0;
-input =[]
-for i in range(0,5):
-    temp =[]
-    for j in range(0,5):
-        temp.append(c)
-        c = c+1;
-    input.append(temp)
+custom_kernel = [[0.5,0.5,0.5] ,[1,0,-1],[0.5,0.5,0.5]]
 
-custom_kernel = [[0,0,0] ,[1,0,-1],[0,0,0]]
+@pytest.mark.parametrize("input_size, kernel, padding, stride", [(5, custom_kernel, 1, 1),(50, custom_kernel, 6, 4) ])
+def test_Devito_pyTorch(input_size, kernel, padding, stride):
+    c=0;
+    input =[]
+    for i in range(0,input_size):
+        temp =[]
+        for j in range(0,input_size):
+            temp.append(c)
+            c = c+1;
+        input.append(temp)
 
-#@pytest.mark.parametrize("input, kernel, padding, stride", [(input, custom_kernel, 1, 1),(input, custom_kernel, 2, 1) ])
-def compare_Devito_pyTorch(input, kernel, padding, stride):
-
-    result_devito = conv_devito_2D_defaultStride(input, custom_kernel, stride, padding)
-
-
-    result_torch = pyTorch_conv(input, custom_kernel, stride, padding)
-
-    print (result_devito)
-
-    print (result_torch)
-
-    print(np.allclose(result_devito, result_torch))
+    result_devito = conv_devito_2D(input, kernel, stride, padding)
 
 
-compare_Devito_pyTorch(input,custom_kernel,padding=2,stride=1)
+    result_torch = pyTorch_conv(input, kernel, stride, padding)
+
+    #print ("devito",result_devito)
+
+    #print ("torch",result_torch[0][0])
+
+    assert(np.allclose(result_devito, result_torch))
+
+
--- tests/2D_conv_tests/Devito_Conv_2d.py
+++ tests/2D_conv_tests/Devito_Conv_2d.py
@@ -1,6 +1,6 @@
 import devito
 from devito import Grid, Function, TimeFunction, Constant, Eq, Inc, \
-    ConditionalDimension, SpaceDimension, Operator
+    ConditionalDimension, SpaceDimension, IncrDimension, Operator
 from matplotlib.pyplot import grid
 import numpy as np
 from sympy import Sum, factor, summation
@@ -26,8 +26,8 @@ def conv_devito_2D (input,kernel,stride,padding, print_code = True):
 
     gridK = Grid(shape=(kernel_size,kernel_size), dimensions =(SpaceDimension("kernel_d1"),SpaceDimension("kernel_d2")))
 
-    gridR = Grid(shape=(result_size,result_size), dimensions =(ConditionalDimension("r_d1",parent=i_1, factor = stride),ConditionalDimension("r_d2",parent=i_2, factor = stride)))
 
+    gridR = Grid(shape=(result_size,result_size), dimensions =(SpaceDimension("r_d1"),SpaceDimension("r_d2")))
 
 
     # input data function
@@ -54,6 +54,7 @@ def conv_devito_2D (input,kernel,stride,padding, print_code = True):
     off_sets = [-1,0,1]
     if padding ==0:
         off_sets = [0,1,2]
+    off_sets = [0,1,2]
 
     k1_offsets = [k1+x for x in off_sets]
 
@@ -65,9 +66,9 @@ def conv_devito_2D (input,kernel,stride,padding, print_code = True):
 
     w = sp.Matrix([K[x] for x in k_indicies]) 
 
-    r1_offsets = [(r1*stride+x) for x in off_sets]
+    r1_offsets = [(r1*stride+x)-padding for x in off_sets]
 
-    r2_offsets = [r2*stride+x for x in off_sets]
+    r2_offsets = [r2*stride+x -padding for x in off_sets]
 
     r_indicies = product(r1_offsets,r2_offsets)
 
@@ -90,86 +91,170 @@ def conv_devito_2D (input,kernel,stride,padding, print_code = True):
 
     return R.data
 
-def conv_devito_2D_defaultStride(input,kernel,stride,padding, print_code = True):
 
-    input_size = len(input)
+# def conv_devito_2D_incr (input,kernel,stride,padding, print_code = True):
 
-    kernel_size = len(kernel)
+#     input_size = len(input)
 
-
-    result_size = (input_size-kernel_size + 2 *padding)//stride + 1 
+#     kernel_size = len(kernel)
 
 
-    i_1 = SpaceDimension("input_d1")
+#     result_size = (input_size-kernel_size + 2 *padding)//stride + 1 
+
+
+  
+#     i_1 = SpaceDimension("input_d1")
+
+#     i_2 = SpaceDimension("input_d2")
+
+#     inr_1 = IncrDimension("in_1", parent=i_1, _min =0, _max = input_size, step = stride)
+
+#     inr_2 = IncrDimension("in_2", parent=i_2, _min =0, _max = input_size, step = stride)
+
+#     gridI = Grid(shape=(input_size,input_size), dimensions =(i_1,i_2))
+
+#     gridK = Grid(shape=(kernel_size,kernel_size), dimensions =(SpaceDimension("kernel_d1"),SpaceDimension("kernel_d2")))
+
+   
+#     gridR = Grid(shape=(result_size,result_size), dimensions =(SpaceDimension("r_d1"),SpaceDimension("r_d2")))
+#     gridR = Grid(shape=(result_size,result_size), dimensions =(ConditionalDimension("r_d1",parent=inr_1, factor = stride), ConditionalDimension("r_d2",parent=inr_2, factor = stride)))
+
+
+
+#     # input data function
+#     I = Function(name="Input_F", grid=gridI, shape=(input_size,input_size), dimensions = (inr_1, inr_2), space_order= padding, dtype=np.float64)
+
+#     # function for kernel
+#     K = Function(name="Kernel_F", grid=gridK, space_order=0, dtype=np.float64)
+
+#     # Result for convolution
+#     R = Function(name="Result_F", grid=gridR, space_order=0, dtype=np.float64)
+
+#     #i for input dimension, k for kernel dimension, r for result dimension (2D array)
+#     i1, i2 = I.dimensions
+#     r1, r2 = R.dimensions
+#     k1, k2 = K.dimensions
+
+
+#     I.data [:] = input
+
+#     K.data [:]= kernel
+
+#     R.data [:]= 0
+
+#     off_sets = [0,1,2]
+
+#     k1_offsets = [k1+x for x in off_sets]
+
+#     k2_offsets = [k2+x for x in off_sets]
+
+#     k_indicies = product(k1_offsets,k2_offsets)
+
+
+
+#     w = sp.Matrix([K[x] for x in k_indicies]) 
+
+#     r1_offsets = [i1+x-padding for x in off_sets]
+
+#     r2_offsets = [i2+x-padding for x in off_sets]
+
+#     r_indicies = product(r1_offsets,r2_offsets)
+
+#     r = sp.Matrix([I[x] for x in r_indicies])
+
+
+#     sten = w.dot(r)
+
+
+#     eq = Eq(R[r1,r2], sten)
+
+#     op = Operator(eq, opt="noop")
+
+#     op.apply()
+
+#     if print_code:
+#         print(op.ccode)
+
+
+#     return R.data   
+
+
+
+# def conv_devito_2D_defaultStride(input,kernel,stride,padding, print_code = True):
+
+#     input_size = len(input)
+
+#     kernel_size = len(kernel)
+
+
+#     result_size = (input_size-kernel_size + 2 *padding)//stride + 1 
+
+
+#     i_1 = SpaceDimension("input_d1")
     
-    i_2 = SpaceDimension("input_d2")
+#     i_2 = SpaceDimension("input_d2")
 
-    gridI = Grid(shape=(input_size,input_size), dimensions =(i_1,i_2))
+#     gridI = Grid(shape=(input_size,input_size), dimensions =(i_1,i_2))
 
-    gridK = Grid(shape=(kernel_size,kernel_size), dimensions =(SpaceDimension("kernel_d1"),SpaceDimension("kernel_d2")))
+#     gridK = Grid(shape=(kernel_size,kernel_size), dimensions =(SpaceDimension("kernel_d1"),SpaceDimension("kernel_d2")))
 
-    gridR = Grid(shape=(result_size,result_size), dimensions =(SpaceDimension("r_d1"),SpaceDimension("r_d2")))
-
-
-
-    # input data function
-    I = Function(name="Input_F", grid=gridI, space_order= padding, dtype=np.float64)
-
-    # function for kernel
-    K = Function(name="Kernel_F", grid=gridK, space_order=0, dtype=np.float64)
-
-    # Result for convolution
-    R = Function(name="Result_F", grid=gridR, space_order=0, dtype=np.float64)
-
-    #i for input dimension, k for kernel dimension, r for result dimension (2D array)
-    i1, i2 = I.dimensions
-    r1, r2 = R.dimensions
-    k1, k2 = K.dimensions
-
-
-    I.data [:] = input
-
-    K.data [:]= kernel
-
-    R.data [:]= 0
-
-    off_sets = [-1,0,1]
-    if padding ==0:
-        off_sets = [0,1,2]
-
-
-    off_sets = [-1,0,1]
-    k1_offsets = [k1+x for x in off_sets]
-
-    k2_offsets = [k2+x for x in off_sets]
-
-    k_indicies = product(k1_offsets,k2_offsets)
+#     gridR = Grid(shape=(result_size,result_size), dimensions =(SpaceDimension("r_d1"),SpaceDimension("r_d2")))
 
 
 
-    w = sp.Matrix([K[x] for x in k_indicies]) 
+#     # input data function
+#     I = Function(name="Input_F", grid=gridI, space_order= padding, dtype=np.float64)
 
-    r1_offsets = [r1+x for x in off_sets]
+#     # function for kernel
+#     K = Function(name="Kernel_F", grid=gridK, space_order=0, dtype=np.float64)
 
-    r2_offsets = [r2+x for x in off_sets]
+#     # Result for convolution
+#     R = Function(name="Result_F", grid=gridR, space_order=0, dtype=np.float64)
 
-    r_indicies = product(r1_offsets,r2_offsets)
-
-
-    r = sp.Matrix([I[x] for x in r_indicies])
-
-
-    sten = w.dot(r)
-
-
-    eq = Eq(R[r1,r2], sten)
-
-    op = Operator(eq, opt="noop")
-
-    op.apply()
-
-    if print_code:
-        print(op.ccode)
+#     #i for input dimension, k for kernel dimension, r for result dimension (2D array)
+#     i1, i2 = I.dimensions
+#     r1, r2 = R.dimensions
+#     k1, k2 = K.dimensions
 
 
-    return R.data   
+#     I.data [:] = input
+
+#     K.data [:]= kernel
+
+#     R.data [:]= 0
+
+#     off_sets = [0,1,2]
+
+#     k1_offsets = [k1+x for x in off_sets]
+
+#     k2_offsets = [k2+x for x in off_sets]
+
+#     k_indicies = product(k1_offsets,k2_offsets)
+
+
+
+#     w = sp.Matrix([K[x] for x in k_indicies]) 
+
+#     r1_offsets = [r1+x-padding for x in off_sets]
+
+#     r2_offsets = [r2+x-padding for x in off_sets]
+
+#     r_indicies = product(r1_offsets,r2_offsets)
+
+#     r = sp.Matrix([I[x] for x in r_indicies])
+
+
+#     sten = w.dot(r)
+
+
+#     eq = Eq(R[r1,r2], sten)
+
+#     op = Operator(eq, opt="noop")
+
+#     op.apply()
+
+#     if print_code:
+#         print(op.ccode)
+
+
+#     return R.data   
