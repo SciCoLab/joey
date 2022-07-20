412d6989b132c278558ea680aa0996f166599f2e
utkarsh8028 <utkarsh8028@gmail.com>
2022-07-20 23:47:56 +0000

    Generalized the new conv method for kernel_size, channels, kernel batch, input_batch & mod. resp unit tests

--- tests/2D_conv_tests/pyTorch_2Dconv.py
+++ tests/2D_conv_tests/pyTorch_2Dconv.py
@@ -4,26 +4,28 @@ import torch
 import joey as ml
 from joey.net import Net
 
+'''
+    Input should be a 3D matrix like 3x512x512 image where 3 is the channels and 512x512 is the size of image.
+    Similary kernel should be be 3D matrix where 3x5x5, 3 is the channel which 
+    should always match the channels in input image, 5x5 is the kernel filter.
+    Assumptioms - kernel would be of square size like 5x5, 3x3, same goes for image
+'''
 
-def pyTorch_conv(input, kernel, stride, padding):
+def pyTorch_conv(input, kernel, padding, stride):
 
-    weights = torch.randn(1,1, 3, 3)
-    weights[0][0] = torch.Tensor(kernel)
+    weights = torch.randn(len(kernel),len(kernel[0]), len(kernel[0][0]), len(kernel[0][0]))
+    weights = torch.Tensor(kernel)
 
-    custom_input_T = torch.randn(1, 1, len(input), len(input))
+    custom_input_T = torch.randn(len(input), len(input[0]), len(input[0][0]), len(input[0][0]))
 
+    custom_input_T= torch.Tensor(input);
 
-    custom_input_T[0][0]= torch.Tensor(input);
-
-
-    torch_conv_op = torch.nn.Conv2d(1,1, kernel_size=len(kernel), padding=padding , stride=stride)
-
-
+    torch_conv_op = torch.nn.Conv2d(len(input),len(kernel), kernel_size=len(kernel), padding=padding , stride=stride)
 
     with torch.no_grad():
         
         torch_conv_op.weight = torch.nn.Parameter(weights)
-        torch_conv_op.bias = torch.nn.Parameter(torch.Tensor([0]))
+        torch_conv_op.bias = torch.nn.Parameter(torch.Tensor([0]*len(kernel)))
 
     result = torch_conv_op(custom_input_T).detach().numpy();
 
