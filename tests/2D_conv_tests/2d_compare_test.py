2cc399c31899570804b20f95cf696e2eec66f131
utkarsh8028 <utkarsh8028@gmail.com>
2022-07-23 16:29:30 +0000

    Cleaned 2d_conv_tests

--- tests/2D_conv_tests/2d_compare_test.py
+++ tests/2D_conv_tests/2d_compare_test.py
@@ -1,12 +1,13 @@
+from asyncio import FastChildWatcher
 import pytest
 from pyTorch_2Dconv import pyTorch_conv
-from Devito_Conv_2d import conv_devito_2D,conv_devito_2D_channels
+from Devito_Conv_2d import conv_devito_2D
 import numpy as np
 
 custom_kernel = [[0,0,0] ,[1,0,-1],[0,0,0]]
 
 @pytest.mark.parametrize("input_size, kernel, padding", [(5, custom_kernel, 2),(50, custom_kernel, 6) ])
-def test_Devito_pyTorch_padding(input_size, kernel, padding ):
+def test_Devito_pyTorch_padding(input_size, kernel, padding, print_results = False ):
     c=0;
     input =[]
     for i in range(0,input_size):
@@ -16,15 +17,18 @@ def test_Devito_pyTorch_padding(input_size, kernel, padding ):
             c = c+1;
         input.append(temp)
 
-    result_devito = conv_devito_2D(input, kernel, padding,1)
+ #to simulate kernel, image with channel 1
     kernel = [kernel]
     input =[input]
 
+    result_devito = conv_devito_2D(input, kernel, padding,1, print_code = print_results)
     result_torch = pyTorch_conv(input, kernel, padding,1)
 
-    #print ("devito",result_devito)
+    if print_results:
+        print ("torch",result_torch[0])
 
-    #print ("torch",result_torch[0][0])
+        print ("devito",result_devito)
+
 
     assert(np.allclose(result_devito, result_torch))
 
@@ -32,7 +36,7 @@ def test_Devito_pyTorch_padding(input_size, kernel, padding ):
 
 
 @pytest.mark.parametrize("input_size, kernel, stride", [(5, custom_kernel, 2),(50, custom_kernel, 5) ])
-def test_Devito_pyTorch_stride(input_size, kernel, stride ):
+def test_Devito_pyTorch_stride(input_size, kernel, stride, print_results = False ):
     c=0;
     input =[]
     for i in range(0,input_size):
@@ -42,15 +46,18 @@ def test_Devito_pyTorch_stride(input_size, kernel, stride ):
             c = c+1;
         input.append(temp)
 
-    result_devito = conv_devito_2D(input, kernel, 1, stride)
+    #to simulate kernel, image with channel 1
     kernel = [kernel]
     input =[input]
 
+    result_devito = conv_devito_2D(input, kernel, 1, stride, print_code = print_results)
+
     result_torch = pyTorch_conv(input, kernel, 1, stride)
 
-    #print ("devito",result_devito)
+    if print_results:
+        print ("torch",result_torch[0])
 
-    #print ("torch",result_torch[0][0])
+        print ("devito",result_devito)
 
     assert(np.allclose(result_devito, result_torch))
 
@@ -59,40 +66,8 @@ def test_Devito_pyTorch_stride(input_size, kernel, stride ):
 
 custom_kernel1 = [[0.5,0.5,0.5,0.5,0.5] ,[1,0,-1,0.5,0.5],[0.5,0.5,0.5,0.5,0.5],[1,0,-1,0.5,0.5],[0.5,0.5,0.5,0.5,0.5]]
 
-@pytest.mark.parametrize("input_size, kernel, padding, stride", [(5, custom_kernel, 1, 1),(50, custom_kernel1, 6, 4) ])
-def test_Devito_pyTorch(input_size, kernel, padding, stride):
-    c=0;
-    input =[]
-    for i in range(0,input_size):
-        temp =[]
-        for j in range(0,input_size):
-            temp.append(c)
-            c = c+1;
-        input.append(temp)
-        
-    print("padding is ", padding)
-    print("stride is ", stride)
-
-
-    result_devito = conv_devito_2D(input, kernel, padding, stride)
-
-    kernel = [kernel]
-    input =[input]
-    result_torch = pyTorch_conv(input, kernel, padding, stride)
-
-    print ("devito \n",result_devito,"\n")
-
-    print ("torch \n",result_torch[0][0])
-
-    print("Do they match", np.allclose(result_devito, result_torch))
-
-    assert(np.allclose(result_devito, result_torch))
-
-
-
-
 @pytest.mark.parametrize("input_size, kernel, channels, padding, stride", [(5, custom_kernel, 1, 1, 1),(50, custom_kernel1, 1, 6, 4) ])
-def test_Devito_pyTorch_channels(input_size,kernel, channels, padding, stride):
+def test_Devito_pyTorch(input_size,kernel, channels, padding, stride, print_results = False):
     c=1;
     input =[]
     for i in range(0,input_size):
@@ -101,7 +76,7 @@ def test_Devito_pyTorch_channels(input_size,kernel, channels, padding, stride):
             temp.append(c)
             c = c+1;
         input.append(temp)   
-    
+    #to simulate kernel, image with channel as specified
     channel_kernel = []
     channel_input =[]
     for i in range(0,channels):
@@ -113,16 +88,13 @@ def test_Devito_pyTorch_channels(input_size,kernel, channels, padding, stride):
 
     result_torch = pyTorch_conv(channel_input, channel_kernel, padding, stride)
 
-    result_devito = conv_devito_2D_channels(channel_input, channel_kernel, padding, stride)
+    result_devito = conv_devito_2D(channel_input, channel_kernel, padding, stride, print_code = print_results)
+    if print_results:
+        print ("torch",result_torch[0])
 
-    print ("torch",result_torch[0])
-
-    print ("devito",result_devito)
+        print ("devito",result_devito)
 
 
     print("Do they match", np.allclose(result_devito, result_torch))
 
-    #assert(np.allclose(result_devito, result_torch))
-#test_Devito_pyTorch(5,custom_kernel,4,2)
-
-test_Devito_pyTorch_channels(5,custom_kernel,3,4,2)
+    assert(np.allclose(result_devito, result_torch))
--- tests/2D_conv_tests/Devito_Conv_2d.py
+++ tests/2D_conv_tests/Devito_Conv_2d.py
@@ -8,58 +8,16 @@ from sympy import Sum, factor, summation
 import sympy as sp
 from itertools import product
 
-def conv_devito_2D (input,kernel,padding,stride, print_code = True):
-
-    input_size = len(input)
-    kernel_size = len(kernel)
-    result_size = (input_size-kernel_size + 2 *padding)//stride + 1 
-
-    # input data function
-    I = Function(name="Input_F",shape=(input_size,input_size), dimensions =(SpaceDimension("input_d1"),SpaceDimension("input_d2")), space_order= padding, dtype=np.float64)
-
-    # function for kernel
-    K = Function(name="Kernel_F", shape=(kernel_size,kernel_size), dimensions =(SpaceDimension("kernel_d1"),SpaceDimension("kernel_d2")), space_order=0, dtype=np.float64)
-
-    # Result for convolution
-    R = Function(name="Result_F", shape=(result_size,result_size), dimensions =(SpaceDimension("r_d1"),SpaceDimension("r_d2")), space_order=0, dtype=np.float64)
-
-    #i for input dimension, k for kernel dimension, r for result dimension (2D array)
-    r1, r2 = R.dimensions
-
-    I.data [:] = input
-    K.data [:]= kernel
-    R.data [:]= 0
 
 
-    off_sets = [x  for x in range(0,kernel_size )]
+'''
+    Input should be a 3D matrix like 3x512x512 image where 3 is the channels and 512x512 is the size of image.
+    Similary kernel should be be 3D matrix where 3x5x5, 3 is the channel which 
+    should always match the channels in input image, 5x5 is the kernel filter.
+    Assumptioms - kernel would be of square size like 5x5, 3x3, same goes for image
+'''
 
-    k1_offsets = k2_offsets = [x for x in off_sets]
-
-    k_indicies = product(k1_offsets,k2_offsets)
-
-    w = sp.Matrix([K[x] for x in k_indicies]) 
-
-    r1_offsets = [(r1*stride+x)-padding for x in off_sets]
-    r2_offsets = [r2*stride+x -padding for x in off_sets]
-
-    r_indicies = product(r1_offsets,r2_offsets)
-
-    r = sp.Matrix([I[x] for x in r_indicies])
-
-    sten = w.dot(r)
-
-    eq = Eq(R[r1,r2], sten)
-
-    op = Operator(eq, opt="noop")
-    op.apply()
-
-    if print_code:
-        print(op.ccode)
-
-    return R.data
-
-
-def conv_devito_2D_channels (input,kernel, padding,stride, print_code = True):
+def conv_devito_2D(input,kernel, padding,stride, print_code = True):
 
 
     channels = len(kernel)
@@ -80,7 +38,7 @@ def conv_devito_2D_channels (input,kernel, padding,stride, print_code = True):
     r1, r2 = R.dimensions
     I.data [:] = input
     K.data [:]= kernel
-    R.data [:]= 10
+    R.data [:]= 0
 
     off_sets = [x  for x in range(0,kernel_size )]
     off_sets_channels = [x  for x in range(0,channels )]
@@ -97,8 +55,6 @@ def conv_devito_2D_channels (input,kernel, padding,stride, print_code = True):
     i3_offsets = [x for x in off_sets_channels]
 
     r_indicies = product(i3_offsets,r1_offsets,r2_offsets)
-
-
     r = sp.Matrix([I[x] for x in r_indicies])
 
     sten = w.dot(r)
