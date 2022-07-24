68e05c11d5d5112cf0872680a7944bce6f6fc572
utkarsh8028 <utkarsh8028@gmail.com>
2022-07-24 18:21:09 +0000

    Added newlayer for conv2_d

--- joey/__init__.py
+++ joey/__init__.py
@@ -1,4 +1,5 @@
 from joey.activation import Activation  # noqa
 from joey.base import *  # noqa
 from joey.layers import *  # noqa
+from joey.layers_v2 import *  # noqa
 from joey.net import *  # noqa
