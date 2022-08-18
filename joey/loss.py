f144baa2405ded48facf9a116808fb49309e25bd
utkarsh8028 <utkarsh8028@gmail.com>
2022-08-18 14:54:29 +0000

    added loss

---
+++ joey/loss.py
@@ -0,0 +1,76 @@
+from joey import Layer
+
+
+
+class _AbstractDiceLoss(Layer):
+    """
+    Base class for different implementations of Dice loss.
+    """
+
+    def __init__(self, weight=None, sigmoid_normalization=True):
+        super(_AbstractDiceLoss, self).__init__()
+        self.register_buffer('weight', weight)
+        self.classes = None
+        self.skip_index_after = None
+        # The output from the network during training is assumed to be un-normalized probabilities and we would
+        # like to normalize the logits. Since Dice (or soft Dice in this case) is usually used for binary data,
+        # normalizing the channels with Sigmoid is the default choice even for multi-class segmentation problems.
+        # However if one would like to apply Softmax in order to get the proper probability distribution from the
+        # output, just specify sigmoid_normalization=False.
+        if sigmoid_normalization:
+            self.normalization = nn.Sigmoid()
+        else:
+            self.normalization = nn.Softmax(dim=1)
+
+    def dice(self, input, target, weight):
+        # actual Dice score computation; to be implemented by the subclass
+        raise NotImplementedError
+
+    def skip_target_channels(self, target, index):
+        """
+        Assuming dim 1 is the classes dim , it skips all the indexes after the desired class
+        """
+        assert index >= 2
+        return target[:, 0:index, ...]
+
+    def forward(self, input, target):
+        """
+        Expand to one hot added extra for consistency reasons
+        """
+        target = expand_as_one_hot(target.long(), self.classes)
+
+        assert input.dim() == target.dim() == 5, "'input' and 'target' have different number of dims"
+
+        if self.skip_index_after is not None:
+            before_size = target.size()
+            target = self.skip_target_channels(target, self.skip_index_after)
+            print("Target {} after skip index {}".format(before_size, target.size()))
+
+        assert input.size() == target.size(), "'input' and 'target' must have the same shape"
+        # get probabilities from logits
+        input = self.normalization(input)
+
+        # compute per channel Dice coefficient
+        per_channel_dice = self.dice(input, target, weight=self.weight)
+
+        loss = (1. - torch.mean(per_channel_dice))
+        per_channel_dice = per_channel_dice.detach().cpu().numpy()
+
+        # average Dice score across all channels/classes
+        return loss, per_channel_dice
+
+
+
+class DiceLoss(_AbstractDiceLoss):
+    """Computes Dice Loss according to https://arxiv.org/abs/1606.04797.
+    For multi-class segmentation `weight` parameter can be used to assign different weights per class.
+    """
+
+    def __init__(self, classes=4, skip_index_after=None, weight=None, sigmoid_normalization=True ):
+        super().__init__(weight, sigmoid_normalization)
+        self.classes = classes
+        if skip_index_after is not None:
+            self.skip_index_after = skip_index_after
+
+    def dice(self, input, target, weight):
+        return compute_per_channel_dice(input, target, weight=self.weight)
